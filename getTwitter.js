dc7913722d186357c1428fad796e4554bd79d5d8
Jackson Chen <tiger2chen@gmail.com>
2016-04-02 01:14:36 -0600

    Remove commented out lines in gettwitter

--- getTwitter.js
+++ getTwitter.js
@@ -1,13 +1,4 @@
-// var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
-
-// var options = {
-//   host: 'https://api.twitter.com',
-//   path: '/1.1/search/tweets.json?q=%23berniesanders&src=typd',
-//   headers: {Authorization: OAuth oauth_consumer_key="UDA0b0ZogUZzk2PAFlBKcaPqB", oauth_nonce="762cbf613d55c289e1659ac49c8a2a60", oauth_signature="aUcszI8A3bJK7fflNQykOVO6x7k%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1459576199", oauth_version="1.0"}
-// }
-
-// var command = "--header "
 
 function getTweets(callback) {
   var child = spawn('curl', [
