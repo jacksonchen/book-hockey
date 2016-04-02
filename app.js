93d13aec1654316b91a58984084f20b4c52f5585
Suyog Soti <ssoti2201@bvsd.org>
2016-04-01 23:41:05 -0600

    connected to the index.html

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 // import express
 var express = require('express');
+// fs file reader
+var fs = require('fs');
 // define an instance of express
 var app = express();
 // create a server with express and http
 var server = require('http').createServer(app).listen(8080);
 // import socket.io and connect it with the server
-var io = require('socket.io');
+var io = require('socket.io')(server);
+// make it so that index.html can excess public files
+app.use(express.static(__dirname + "/public"));
+// route the people with this
+app.get("/", function(req, res) {
+    res.writeHead(200);
+    res.end(fs.readFileSync('index.html'));
+});
+
+io.on('connection', function(socket){
+    console.log("A program connected");
+});
