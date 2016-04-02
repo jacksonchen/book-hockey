4b9c1be3baab75d31c45f186ef0ef2ffe3e12540
Suyog Soti <ssoti2201@bvsd.org>
2016-04-02 03:25:19 -0600

    made it update on emotions event

--- public/script.js
+++ public/script.js
@@ -1,1 +1,15 @@
-var socket = io('http://localhost:8080');
+$(document).ready(function(){
+    var socket = io('http://localhost:8080');
+
+    socket.on('emotions', function(emotions) {
+        var sum = 0;
+        for (e in emotions){
+            console.log(e, emotions[e]);
+            if(emotions.hasOwnProperty(e)){
+                sum += emotions[e];
+            }
+        }
+        $('#wrapper').text(JSON.stringify(emotions));
+    });
+
+});
