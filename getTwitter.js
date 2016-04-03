630d8fce1a7eca9b57f314fa3221e23346da777c
Jackson Chen <tiger2chen@gmail.com>
2016-04-02 19:51:31 -0600

    Update api key

--- getTwitter.js
+++ getTwitter.js
@@ -4,7 +4,7 @@ function getTweets(callback) {
   var child = spawn('curl', [
     '--get', 'https://api.twitter.com/1.1/search/tweets.json',
     '--data', 'count=100&q=%23berniesanders&src=typd',
-    '--header', 'Authorization: OAuth oauth_consumer_key=\"UDA0b0ZogUZzk2PAFlBKcaPqB\", oauth_nonce=\"9ec7e6ed80103cb5c476e3b611faf80f\", oauth_signature=\"WnA6Lp94QQSilsLZCGTgra6MCgo%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1459579286\", oauth_version=\"1.0\"'
+    '--header', 'Authorization: OAuth oauth_consumer_key=\"UDA0b0ZogUZzk2PAFlBKcaPqB\", oauth_nonce=\"80fed41a8537395e237d9f0a0ec8fddb\", oauth_signature=\"Eor3wUueJBSeB4LZaksaKXaVeBI%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1459647859\", oauth_token=\"2922199336-aPKUBmP1kPlIN60WQJkZTh33E9nksBqau19bOVn\", oauth_version=\"1.0\"'
   ]);
 
   var tweetsHash = '';
@@ -21,7 +21,6 @@ function getTweets(callback) {
 exports.tweets = function compileTweets(hash, callback) {
   var tweetArr = []
   getTweets(function(tweetsHash) {
-    //   console.log(tweetsHash);
     tweetsHash.forEach(function(tweet) {
       tweetArr.push(tweet.text);
     });
@@ -30,7 +29,7 @@ exports.tweets = function compileTweets(hash, callback) {
 }
 
 // MAIN
-//
-// compileTweets(function(tweetArr) {
+
+// compileTweets("hi", function(tweetArr) {
 //   console.log(tweetArr);
 // })
